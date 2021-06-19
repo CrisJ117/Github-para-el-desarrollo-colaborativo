c7483efb8f1fe2ed0569da1fd6950e2a04897ffe
ƈʀɨֆ <86115235+CrisJ117@users.noreply.github.com>
2021-06-19 18:50:52 -0500

    Update ActivadadB2.cpp

--- ActivadadB2.cpp
+++ ActivadadB2.cpp
@@ -7,7 +7,7 @@ float matriz1[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
 float matriz2[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
 float suma[10],resta[10],producto[10],division[10];
 
-//El primer problema es resulto por Kevin Tapuy Cañla
+//El primer problema es resulto por Kevin Tapuy Cañola
 
 
 return 0;
